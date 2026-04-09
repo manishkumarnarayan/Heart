82095c9727fc88ba45f389fcb569b557c73098b2
manishkumarnarayan <manish117ff@gmail.com>
2026-04-09 18:42:31 +0530

    changes

--- heart.js
+++ heart.js
@@ -2,20 +2,24 @@ let c = ["red", "#9D6381", "hotpink", "aqua", "#B5D99C"]
 let i = 0
 let color = c[i];
 let out = "";
-let s = "I wanna say something;) padh lo madarchod";
+let s = "I wanna say something;) padh lo madarchod" + " ";
 let l = 0;
+function word() {
+    let w = "";
+    while (s[l] != " ") {
+        w += s[l];
+        l++;
+    }
+    l++;
+    return w;
+}
 function co() {
     i++;
     if (i > c.length - 1) {
         i = 0
     }
     if (l < s.length) {
-        if (s[l] == " ") {
-            l++;
-            out += " ";
-        }
-        out += s[l];
-        l++;
+        out += word();
     }
     else {
         l = 0;
