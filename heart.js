2fd3b8d072b17d002a3109350af4516ce1a7d190
manishkumarnarayan <manish117ff@gmail.com>
2026-04-09 18:33:48 +0530

    string changed

--- heart.js
+++ heart.js
@@ -1,27 +1,27 @@
-let c=["red","#9D6381","hotpink","aqua","#B5D99C"]
-let i=0
-let color=c[i];
+let c = ["red", "#9D6381", "hotpink", "aqua", "#B5D99C"]
+let i = 0
+let color = c[i];
 let out = "";
-let s = " I <3 u";
+let s = "I wanna say something;) padh lo madarchod";
 let l = 0;
 function co() {
     i++;
-    if (i>c.length-1) {
-        i=0
+    if (i > c.length - 1) {
+        i = 0
     }
-    if (l<s.length) {
-        if (s[l]==" ") {
+    if (l < s.length) {
+        if (s[l] == " ") {
             l++;
-            out+=" ";
+            out += " ";
         }
-        out+=s[l];
+        out += s[l];
         l++;
     }
-    else{
-        l=0;
-        out="";
+    else {
+        l = 0;
+        out = "";
     }
-    color=c[i]
+    color = c[i]
     document.getElementById("text").innerHTML = out;
     document.getElementById("col1").style.backgroundColor = color;
     document.getElementById("col2").style.backgroundColor = color;
