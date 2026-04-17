949b4923843d72527b90e8bc3b96dde4c470e156
manishkumarnarayan <manish117ff@gmail.com>
2026-04-17 23:48:04 +0530

    FINAL

--- index.js
+++ index.js
@@ -10,7 +10,18 @@ document.addEventListener("DOMContentLoaded", () => {
         }
         c = window.location.href;
         console.log(window.location.href)
-        c = c+"heart.html#"+b;
+        c = c+"heart.html#"+b; 
+        /*
+        soooooooooo it won't work on live server 
+        but it works on github host/pages 
+        because turns out window.location.href
+        returns url with index.html 
+        when hosted with live server locally 
+        whereas when we deploy it via github 
+        same funtions returns url without index.html.
+        So if you have to run it on local live server replace line 13 with 
+        c = c.substring(0,c.indexOf("index.html"))+"heart.html#"+b; 
+        */
         console.log(c)
         document.querySelector("#link").innerHTML = 'Click to copy the link';
         document.querySelector(".link-container").classList.add("active");
